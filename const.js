2e3abaad43f632fdf4951764f8dbebeaa6ab4e23
Rakesh Patel <emailme@rakeshrpatel.com>
2016-09-12 08:16:13 -0400

    updated

--- const.js
+++ const.js
@@ -9,7 +9,8 @@ const WIT_TOKEN = 'DWSEWR32EUMTTQ3LNXNQELNQA4ZL4QS2';
 // }
 
 // Messenger API parameters
-const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+const FB_PAGE_TOKEN = 'EAAQwFrRHYK0BABywxaeFcYUUjG8No6xTtiaFVM8syx4IdUeQ4tGjdUUdHZA3cvcuqjMGUk4srCZAbKKiPqHThRq4gltVLVM0G0zEe4jLbIL77uFDJitoky8S5OFJ4uVncGGbWowmhc1EiZAVsl9A10pm4j0kWgpXQQt2Ff91QZDZD'
+// process.env.FB_PAGE_TOKEN;
 
 var FB_VERIFY_TOKEN = 'just_do_it'
 // process.env.FB_VERIFY_TOKEN;
@@ -22,3 +23,5 @@ module.exports = {
   FB_PAGE_TOKEN: FB_PAGE_TOKEN,
   FB_VERIFY_TOKEN: FB_VERIFY_TOKEN,
 };
+
+// curl -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=EAAQwFrRHYK0BABywxaeFcYUUjG8No6xTtiaFVM8syx4IdUeQ4tGjdUUdHZA3cvcuqjMGUk4srCZAbKKiPqHThRq4gltVLVM0G0zEe4jLbIL77uFDJitoky8S5OFJ4uVncGGbWowmhc1EiZAVsl9A10pm4j0kWgpXQQt2Ff91QZDZD"
